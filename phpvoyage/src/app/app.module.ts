5328c4faee20b57d564b7dd233d691cd00a9b442
Flavien <flavien37500@gmail.com>
2019-09-10 10:14:21 +0200

    creation result

--- phpvoyage/src/app/app.module.ts
+++ phpvoyage/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
