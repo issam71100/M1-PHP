a231ca9c8b63b7759a269d7b998f03f57cc9e39d
Tony PEREIRA <pereira.tony98@gmail.com>
2019-09-10 15:49:19 +0200

    Liaison angular symfony + routing

--- phpvoyage/src/app/app.module.ts
+++ phpvoyage/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CityListComponent } from './city-list/city-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CityListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: CityListComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
