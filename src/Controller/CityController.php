ede9ebb97f6305f633f89df13395b7d9aa4ffdb8
Kadar Issam <issam71100@gmail.com>
2019-09-11 16:04:16 +0200

    fix CityController

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Assets\AppEncoder;
 use App\Entity\City;
 use App\Repository\CityRepository;
+use App\Repository\CountryRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -31,6 +32,37 @@ class CityController extends AbstractController
     }
 
     /**
+     * @Route("/new", name="city_new", methods={"GET","POST"})
+     * @param Request $request
+     * @param AppEncoder $encoder
+     * @param CountryRepository $countryRepository
+     * @return Response
+     */
+    public function new(Request $request, AppEncoder $encoder, CountryRepository $countryRepository): Response
+    {
+        $params = $request->request->all();
+
+        if (!isset($params["name"]) || !isset($params["image"]) || !isset($params["country"])) {
+            return new Response(null, 400, ["Content-Type" => "application/json"]);
+        }
+
+        $country = $countryRepository->findOneByName($params["country"]);
+
+        $city = new City();
+        $city->setName($params["name"]);
+        $city->setImage($params["image"]);
+        $city->setContry($country);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($city);
+        $entityManager->flush();
+
+        $response = $encoder->encoder($country);
+
+        return new Response($response, 200, ["Content-Type" => "application/json"]);
+    }
+
+    /**
      * @Route("/view/{id}", name="city_show", methods={"GET"})
      * @param City $city
      * @param AppEncoder $encoder
@@ -38,33 +70,65 @@ class CityController extends AbstractController
      */
     public function show(City $city, AppEncoder $encoder): Response
     {
-        $cityRep = $this->getDoctrine()
-            ->getRepository(City::class)
-            ->find($city);
-
-        if (!$cityRep) {
-            throw $this->createNotFoundException(
-                'No data found for '.$city
-            );
+        if ($city != null){
+            $response = $encoder->encoder($city);
+            return new Response($response, 200, ["Content-Type" => "application/json"]);
         }
-        $response = $encoder->encoder($city);
-        return new Response($response, 200, ["Content-Type" => "application/json"]);
+
+        return new Response(null, 400, ["Content-Type" => "application/json"]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="country_edit", methods={"GET","POST"})
+     * @param Request $request
+     * @param City $city
+     * @param AppEncoder $encoder
+     * @param CountryRepository $countryRepository
+     * @return Response
+     */
+    public function edit(Request $request, City $city, AppEncoder $encoder, CountryRepository $countryRepository): Response
+    {
+        if ($city != null) {
+            $params = $request->request->all();
+
+            if (!isset($params["name"]) || !isset($params["image"]) || !isset($params["country"])) {
+                return new Response(null, 400, ["Content-Type" => "application/json"]);
+            }
+
+            $country = $countryRepository->findOneByName($params["country"]);
+
+            $city->setName($params["name"]);
+            $city->setImage($params["image"]);
+            $city->setContry($country);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+
+            $response = $encoder->encoder($country);
+            return new Response($response, 200, ["Content-Type" => "application/json"]);
+        }
+
+        return new Response(null, 400, ["Content-Type" => "application/json"]);
     }
 
     /**
      * @Route("/{id}", name="city_delete", methods={"DELETE"})
-     * @param Request $request
+     * @param AppEncoder $encoder
      * @param City $city
      * @return Response
      */
-    public function delete(Request $request, City $city): Response
+    public function delete(AppEncoder $encoder , City $city): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$city->getId(), $request->request->get('_token'))) {
+        if ($city != null) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($city);
             $entityManager->flush();
+
+            $response = $encoder->encoder($city);
+
+            return new Response($response, 200, ["Content-Type" => "application/json"]);
         }
 
-        return $this->redirectToRoute('city_index');
+        return new Response(null, 400, ["Content-Type" => "application/json"]);
     }
 }
