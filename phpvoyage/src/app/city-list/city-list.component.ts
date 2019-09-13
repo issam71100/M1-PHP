d4acfd0ac12f8221cd47f6c2411b0f142f21c005
Tony PEREIRA <pereira.tony98@gmail.com>
2019-09-13 16:17:38 +0200

    Récupération des données du JSON sur le front

--- phpvoyage/src/app/city-list/city-list.component.ts
+++ phpvoyage/src/app/city-list/city-list.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { City } from '../city';
+
+
 
 @Component({
   selector: 'app-city-list',
   templateUrl: './city-list.component.html',
   styleUrls: ['./city-list.component.css']
 })
+
 export class CityListComponent implements OnInit {
   
   constructor(private http : HttpClient) { }
 
+  private cities = [];
+
   ngOnInit() {
-    this.http.get('http://localhost:8000/city').subscribe(data => {
+    this.http.get('http://localhost:8000/city').subscribe((data : any[]) => {
       console.log(data);
+      this.cities = data;
     });
   }
   
