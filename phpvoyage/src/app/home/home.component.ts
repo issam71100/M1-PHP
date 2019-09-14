42064b68efed7caa2102899e68fa6344aa8eb099
Tony PEREIRA <pereira.tony98@gmail.com>
2019-09-14 10:01:34 +0200

    Mise en place de la recherche (50%)

--- phpvoyage/src/app/home/home.component.ts
+++ phpvoyage/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ContinentService } from '../continent.service';
+import { CountryService } from '../country.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private continentservice : ContinentService, private countryservice : CountryService) { }
+
+  private continents = [];
+  private lespays = [];
 
   ngOnInit() {
+
+    this.continentservice.getContinents().subscribe((data : any[]) => {
+      console.log(data);
+      this.continents = data;
+    });;
+
+    this.countryservice.getPays().subscribe((data : any[]) => {
+      console.log(data);
+      this.lespays = data;
+    });;
   }
 
 }
